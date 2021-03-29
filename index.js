493ee6f1080ecd6fc7925b3b02d6ab7af9db30da
NeptunePurpleHeart <54174780+NeptunePurpleHeart@users.noreply.github.com>
2021-03-29 13:21:01 +0300

    Initial commit, readline interface
    Promisify readline.question to work with asynnc/await, created placeholder Hello World script

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
-console.log('123');
-console.log('Hello, World!');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const question = str => new Promise(resolve => rl.question(str, resolve));
+
+async function start() {
+  const answer = await question('What do you want to do?\n');
+  if (answer.toLowerCase() === 'hello') console.log('World');
+  rl.close();
+}
+
+start();
+
+
