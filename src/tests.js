4486d2ed0bfd6f9816c95ca2a0dbdc698e3addde
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-04-25 21:40:10 +0300

    Wrote initial test

---
+++ src/tests.js
@@ -0,0 +1,11 @@
+'use strict';
+const { Crypto } = require('./crypto');
+const assert = require('assert').strict;
+
+const crypto = new Crypto();
+
+(async () => {
+  const result = await crypto.getCurrencyPriceVolume('USD, BTC');
+  assert.match(result, /lowest/);
+  console.log('Passed');
+})();
