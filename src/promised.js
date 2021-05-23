37331d051164cda05f6ddba32df61f244b424d7d
yanulya <vladonyaroshchuk@gmail.com>
2021-05-23 08:45:01 +0300

    Modified regExp[] and decodeString() in promised.js

--- src/promised.js
+++ src/promised.js
@@ -44,18 +44,18 @@ const errorWrapper = handleError => func => (...args) =>
   func(...args).catch(handleError);
 
 const escapeChars = { lt: '<', gt: '>', quot: '"', apos: '\'', amp: '&' };
-const regExp = [/^#x([\da-fA-F]+)$/, /^#(\d+)$/];
+const regExp = [/&([^;]+);/g, /^#x([\da-fA-F]+)$/, /^#(\d+)$/];
 
 function decodeString(str) {
-  return str.replace(/&([^;]+);/g, (entity, entityCode) => {
+  return str.replace(regExp[0], (entity, entityCode) => {
     let match;
     if (entityCode in escapeChars) {
       return escapeChars[entityCode];
-    } else if (entityCode.match(regExp[0])) {
-      match = entityCode.match(regExp[0]);
-      return String.fromCharCode(parseInt(match[1], 16));
     } else if (entityCode.match(regExp[1])) {
       match = entityCode.match(regExp[1]);
+      return String.fromCharCode(parseInt(match[1], 16));
+    } else if (entityCode.match(regExp[2])) {
+      match = entityCode.match(regExp[2]);
       return String.fromCharCode(~~match[1]);
     } else return entity;
   });
