2f8ebca15b5a7fa9fd9036858f7f7b4e2e158103
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-05-26 19:01:29 +0300

    Eslint fix

--- src/wallet.js
+++ src/wallet.js
@@ -7,55 +7,54 @@ const errorHandlerWrapped = promised.errorWrapper(promised.handler);
 const safeGet = errorHandlerWrapped(promised.getRequest);
 const safePost = errorHandlerWrapped(promised.postRequest);
 
-class RawWallet {
-    _token;
-    _keys;
+class Wallet {
+  #token;
+  #keys;
 
-    constructor(currency, token) {
-        this.defaultUrl = 'api.blockcypher.com';
-        this.defaultPath = `/v1/${currency}/main/`;
-        this._token = token;
-    }
+  constructor(currency, token) {
+    this.defaultUrl = 'api.blockcypher.com';
+    this.defaultPath = `/v1/${currency}/main/`;
+    this.#token = token;
+  }
 
-    async createWallet() {
-        const path = this.defaultPath + `addrs?token=`;
-        const data = JSON.stringify({
-            token: this._token,
-        });
-        const options = {
-            method: 'POST',
-            hostname: this.defaultUrl,
-            path,
-            headers: {},
-        };
-        const result = await safePost(options, data);
-        this._keys = result;
-        return result;
+  async createWallet() {
+    const path = this.defaultPath + `addrs?token=`;
+    const data = JSON.stringify({
+      token: this.#token,
+    });
+    const options = {
+      method: 'POST',
+      hostname: this.defaultUrl,
+      path,
+      headers: {},
+    };
+    const result = await safePost(options, data);
+    this.#keys = result;
+    return result;
+  }
+  async getAdrsBalance(adrs) {
+    const path =  `/v1/btc/main/addrs/${adrs}/balance`;
+    const link = 'https://' + this.defaultUrl + path;
+    const result = [];
+    const getInfo = await safeGet(link);
+    result.push(`Total received: ${getInfo.total_received} satoshis`);
+    result.push(`Total send: ${getInfo.total_sent} satoshis`);
+    result.push(`Balance: ${getInfo.balance} satoshis`);
+    console.log(`${result.join('\n')}\n`);
+    return result;
+  }
+  get keys() {
+    if (!this.#keys) return null;
+    const walletInfo = [];
+    const keys = Object.keys(this.#keys);
+    for (const key of keys) {
+      walletInfo.push(`${key}:${this.#keys[key]}\n`);
     }
-    async getAdrsBalance(adrs) {
-        const path =  `/v1/btc/main/addrs/${adrs}/balance`;
-        const link = 'https://'+this.defaultUrl+path;
-        let result = [];
-        const getInfo = await safeGet(link);
-        result.push(`Total received: ${getInfo.total_received} satoshis`);
-        result.push(`Total send: ${getInfo.total_sent} satoshis`);
-        result.push(`Balance: ${getInfo.balance} satoshis`)
-        console.log(`${result.join('\n')}\n`);
-        return result;
-    }
-    get keys() {
-        if (!this._keys) return;
-        const walletInfo = [];
-        const keys = Object.keys(this._keys);
-        for (const key of keys) {
-            walletInfo.push(`${key}:${this._keys[key]}\n`);
-        }
-        return walletInfo;
-    }
+    return walletInfo;
+  }
 }
 
-const Wallet = promised.objWrapper(RawWallet, promised.objHandler);
 module.exports = {
-    Wallet
+  Wallet
 };
 
